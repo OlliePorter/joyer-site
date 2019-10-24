cca059359a892bbb0fd0d8d45732a032704e1043
OlliePorter <ol.porter@me.com>
2019-10-24 22:54:17 +0100

    Button and flexbox added to cover

--- src/About.js
+++ src/About.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import Icon from '@material-ui/core/Icon';
 import { makeStyles } from '@material-ui/core/styles';
 import theme from './theme';
 
@@ -12,7 +14,7 @@ const useStyles = makeStyles({
       backgroundSize: 'cover',
       backgroundRepeat: 'no-repeat',
       backgroundPosition: 'center',
-      minHeight: '77vh',
+      minHeight: '80vh',
       display: 'flex',
       flexDirection: 'column',
       alignItems: 'center',
@@ -21,7 +23,16 @@ const useStyles = makeStyles({
     heroOverlay: {
       backgroundColor: 'blue',
       minHeight: '77vh',
-    }
+    },
+    button: {
+      width: '179px',
+      color: 'white',
+      backgroundColor: theme.palette.primary.main,
+      height: '52px',
+      fontSize: '17px',
+      borderRadius: '50px',
+      margin: '40px',
+  }
 });
 
 export default function About() {
@@ -30,12 +41,14 @@ export default function About() {
     return (
     <>
       <div className={classes.heroContent}>
-        <Typography component="h1" variant="h2" align="center" className={classes.title}>
+        <Typography component="h2" variant="h3" align="center" className={classes.title}>
           Love Your Clan.
         </Typography>
         <Typography component="h1" variant="subtitle1" align="center" className={classes.title}>
         JOYER is a wellbeing app which cultivates relationships. It's the best way to maintain strong connections and bring balance to your social headspace.
         </Typography>
+        <Button variant="contained" color="secondary" className={classes.button}>Coming Soon</Button>
+        <Icon>star</Icon>
       </div>
     </>
     );
