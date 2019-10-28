394c792059f6379aa551d80ed42cb916cf8024d9
OlliePorter <ol.porter@me.com>
2019-10-28 23:04:14 +0000

    Add material ui grid to feature component

--- src/Features.js
+++ src/Features.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
 import theme from './theme';
 
 const useStyles = makeStyles({
@@ -16,12 +17,22 @@ const useStyles = makeStyles({
     },
     featureContainer: {
       backgroundColor: theme.palette.secondary.light,
-      minHeight: '959px',
+      // minHeight: '959px',
       paddingTop: '100px',
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      justifyContent: 'flex-start',
+      paddingBottom: '100px',
+      // display: 'flex',
+      // flexDirection: 'column',
+      // alignItems: 'center',
+      // justifyContent: 'flex-start',
+    },
+    innerContainer: {
+      flexGrow: 1,
+      backgroundColor: 'red',
+      marginTop: '100px',
+    },
+    featureText: {
+      padding: theme.spacing(2),
+      textAlign: 'center',
     },
 });
 
@@ -38,6 +49,19 @@ export default function Features() {
       <Typography component="p" align="center" className={classes.subtitle}>
         Just Optimise Your Everyday Relationships.
       </Typography>
+      <div className={classes.innerContainer}>
+      <Grid container>
+         <Grid item xs>
+           <Typography className={classes.featureText}>It's simple...</Typography>
+         </Grid>
+         <Grid item xs>
+           <Typography className={classes.featureText}>It's simple...</Typography>
+         </Grid>
+         <Grid item xs>
+           <Typography className={classes.featureText}>It's simple...</Typography>
+         </Grid>
+       </Grid>
+       </div>
       </div>
     </>
     );
