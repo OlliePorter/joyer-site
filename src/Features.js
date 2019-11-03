e5b9569b3db1f67290caf7e3ad598815f27af459
OlliePorter <ol.porter@me.com>
2019-11-03 21:44:31 +0000

    Add inner flex boxes to feature partial

--- src/Features.js
+++ src/Features.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
+import DirectionsBoatIcon from '@material-ui/icons/DirectionsBoat';
 import Grid from '@material-ui/core/Grid';
 import { ReactComponent as Iphone } from './assets/iphone.svg';
 import theme from './theme';
@@ -23,19 +24,28 @@ const useStyles = makeStyles({
     },
     innerContainer: {
       flexGrow: 1,
-      textAlign: 'center',
+      // textAlign: 'center',
       marginTop: '100px',
+      backgroundColor: 'red',
     },
     featureText: {
       padding: theme.spacing(2),
       textAlign: 'center',
     },
-    featureImage: {
-      marginLeft: 'auto',
-      marginRight: 'auto',
+    featureIcon: {
+      color: 'black',
+      transform: 'scale(1.5)',
+    },
+    featureRow: {
+      display: 'flex',
+      flexDirection: 'row',
+    },
+    featureColumn: {
+      display: 'flex',
+      flexDirection: 'column',
+      // width: '300px',
     }
 });
-
 
 export default function Features() {
   const classes = useStyles();
@@ -43,27 +53,37 @@ export default function Features() {
     return (
     <>
       <div className={classes.featureContainer}>
-      <Typography component="h1" variant="subtitle" align="center" className={classes.title}>
-        It's simple...
-      </Typography>
-      <Typography component="p" align="center" className={classes.subtitle}>
-        Just Optimise Your Everyday Relationships.
-      </Typography>
-      <div className={classes.innerContainer}>
-      <Grid container>
-         <Grid item xs>
-           <Typography className={classes.featureText}>It's simple...</Typography>
-         </Grid>
-         <Grid item xs>
-           <Iphone className={classes.featureImage} />
-         </Grid>
-         <Grid item xs>
-           <Typography className={classes.featureText}>It's simple...</Typography>
-         </Grid>
-       </Grid>
-       </div>
-       <div>
-         </div>
+        <Typography component="h2" variant="h4" align="center" className={classes.title}>
+          It's simple...
+        </Typography>
+        <Typography component="p" align="center" className={classes.subtitle}>
+          Just Optimise Your Everyday Relationships.
+        </Typography>
+        <div className={classes.innerContainer}>
+          <Grid container>
+            <Grid item xs>
+              <div className={classes.featureRow}>
+                <div className={classes.featureColumn}>
+                <Typography component="h2" variant="h5" className={classes.title}>
+                Start your Joyage
+                </Typography>
+                <Typography component="p" className={classes.subtitle}>
+                It all starts with your personalised social journey (or 'Joyage'). Navigate weekly challenges in your Joyage to see more friends & earn rewards.
+                </Typography>
+                </div>
+                <div className={classes.featureColumn}>
+                <DirectionsBoatIcon className={classes.featureIcon}/>
+                </div>
+              </div>
+            </Grid>
+            <Grid item xs>
+              <Iphone className={classes.featureImage} />
+            </Grid>
+            <Grid item xs>
+              <Typography className={classes.featureText}>It's simple...</Typography>
+            </Grid>
+          </Grid>
+        </div>
       </div>
     </>
     );
