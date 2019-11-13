c01e31aff37dc20e2f9b1c02d4c7ab831c0632c1
OlliePorter <ol.porter@me.com>
2019-11-13 23:12:32 +0000

    Add navbar text & style button

--- src/Header.js
+++ src/Header.js
@@ -4,9 +4,10 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Logo from "./assets/JOYER_APP_LOGO.png";
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles(theme => ({
-  navContainer: {
+  outerNavContainer: {
     position: 'absolute',
     backgroundColor: 'transparent',
     zIndex: 100,
@@ -16,20 +17,45 @@ const useStyles = makeStyles(theme => ({
     borderBottomWidth: 0,
     height: '70px',
     display: 'flex',
+    justifyContent: 'space-around',
+  },
+  innerNavContainer: {
+    display: 'flex',
+    justifyContent: 'right',
+    flexDirection: 'row',
   },
   joyerLogo: {
     height: '35px',
     width: '30px',
     paddingTop: '2em',
-    paddingLeft: '16em',
+    paddingRight: '16em',
+  },
+  joyerText: {
+    color: 'white',
+    paddingTop: '1.7em',
+    paddingRight: '2em',
+  },
+  navButton: {
+    height: '35px',
+    width: '90px',
+    borderRadius: '20px',
+    textTransform: 'none',
+    color: 'white',
+    borderColor: 'white',
+    marginTop: '2.5em',
   }
 }));
 
 export default function Header() {
   const classes = useStyles();
   return (
-    <nav className={classes.navContainer}>
+    <nav className={classes.outerNavContainer}>
       <img src={Logo} className={classes.joyerLogo}/>
+      <div className={classes.innerNavContainer}>
+      <p className={classes.joyerText}>Home</p>
+      <p className={classes.joyerText}>About Us</p>
+      <Button variant="outlined" href="#outlined-buttons" className={classes.navButton}>Sign Up</Button>
+      </div>
     </nav>
   );
 }
