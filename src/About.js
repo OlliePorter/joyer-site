a72abaebc1f024472a97667fa399bfb594e2ed54
OlliePorter <ol.porter@me.com>
2019-11-17 22:41:24 +0000

    Start about page styling

--- src/About.js
+++ src/About.js
@@ -4,6 +4,24 @@ import { makeStyles } from '@material-ui/core/styles';
 import theme from './theme';
 
 const useStyles = makeStyles({
+  title: {
+    color: theme.palette.primary.light,
+    fontWeight: 300,
+    margin: '20px'
+  },
+  subtitle: {
+    color: theme.palette.primary.light,
+    padding: '8px',
+    fontWeight: 300,
+    width: '60%',
+  },
+  heroContent: {
+    backgroundImage: 'url(/joyer-group.png)',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+    minHeight: '80vh',
+  },
 });
 
 export default function About() {
@@ -11,7 +29,14 @@ export default function About() {
 
     return (
     <>
-      <p>This is the about-us page</p>
+      <div className={classes.heroContent}>
+        <Typography component="h2" variant="h3" align="center" className={classes.title}>
+          Our Mission:
+        </Typography>
+        <Typography component="h2" variant="h6" align="center" className={classes.subtitle}>
+        Promote collective wellbeing through stronger, healthier relationships.
+        </Typography>
+      </div>
     </>
     );
 }
