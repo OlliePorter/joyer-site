f8b47a8c22164b5aaeb0e4f588a9cd71902efc2b
OlliePorter <ol.porter@me.com>
2019-11-17 15:59:07 +0000

    Add functional navbar with hover text state, sign up button & sign up page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import { Link } from 'react-router-dom';
 import {
   Route,
   NavLink,
@@ -8,7 +9,9 @@ import {
 import Home from './Home';
 import About from './About';
 import Header from './Header';
+import SignUp from './SignUp';
 import Logo from "./assets/JOYER_APP_LOGO.png";
+import Button from '@material-ui/core/Button';
 
 function App() {
   return (
@@ -17,13 +20,15 @@ function App() {
         <ul className="outer-nav-container">
             <img src={Logo} className="joyer-logo"/>
             <div className="inner-nav-container">
-            <li><NavLink to="/" className="joyer-text">Home</NavLink></li>
-            <li><NavLink to="/about-us" className="joyer-text">About Us</NavLink></li>
+              <li><NavLink to="/" className="joyer-text">Home</NavLink></li>
+              <li><NavLink to="/about-us" className="joyer-text">About Us</NavLink></li>
+              <li><Button component={Link} variant="outlined" to="/sign-up" className="nav-button">Sign Up</Button></li>
             </div>
           </ul>
-          <div className="content">
+          <div>
             <Route exact path="/" component={Home}/>
             <Route path="/about-us" component={About}/>
+            <Route path="/sign-up" component={SignUp}/>
           </div>
       </div>
     </HashRouter>
