36fbfd7e96c330b942d6b095c9a9cef85fb47cc0
OlliePorter <ol.porter@me.com>
2019-11-25 22:03:20 +0000

    Add text & styling to testimonials page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Cover from './Cover';
 import Features from './Features';
+import Testimonials from './Testimonials';
 
 export default function Home() {
     return (
     <>
       <Cover/>
       <Features/>
+      <Testimonials/>
     </>
     );
 }
