8fc4cb5fd9d1923b7830e815a55725710c7e0934
OlliePorter <ol.porter@me.com>
2020-01-19 21:45:53 +0000

    Add card content with styling

---
+++ src/components/About/TeamCard.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { makeStyles, withTheme } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import theme from '../../theme';
+
+const useStyles = makeStyles({
+  card: {
+    maxWidth: 380,
+  },
+  media: {
+    height: 420,
+  },
+  cardIcon: {
+    opacity: 0.3,
+    width: '20px',
+    "&:hover": {
+      opacity: 1,
+      color: theme.palette.primary.main
+    },
+  },
+  nameStyle: {
+    color: theme.palette.primary.main,
+  },
+  titleStyle: {
+    fontWeight: '300',
+  },
+  bioStyle: {
+    fontWeight: '300',
+  }
+});
+
+export default function TeamCard(props) {
+    const classes = useStyles();
+  
+    return (
+      <Card className={classes.card}>
+        <CardMedia
+          className={classes.media}
+          image={props.image_url}
+          title="Team profile icon"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2" className={classes.nameStyle}>
+            {props.name}
+          </Typography>
+          <Typography gutterBottom component="p" className={classes.titleStyle}>
+            {props.title}
+          </Typography>
+          <Typography color="textSecondary" component="p" className={classes.bioStyle}>
+            {props.bio}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <TwitterIcon className={classes.cardIcon} onClick={()=> window.open(`${props.twitter_link}`, "_blank")}/>
+          <LinkedInIcon className={classes.cardIcon} onClick={()=> window.open(`${props.linked_in_link}`, "_blank")}/>
+        </CardActions>
+      </Card>
+    );
+  }
