60825d95204b889f1dae6562189ded08f7a85bc2
OlliePorter <ol.porter@me.com>
2020-01-25 18:28:40 +0000

    Refactor navbar hover style

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -4,15 +4,33 @@ import { makeStyles } from '@material-ui/core/styles';
 import theme from '../../theme';
 
 const useStyles = makeStyles ({
-
+  signUpContainer: {
+      backgroundImage: 'url(/joyer-group.png)',
+      backgroundSize: 'cover',
+      backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'center',
+      minHeight: '100vh',
+  },
+  signUpContainerOverlay: {
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    position: 'absolute',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    minHeight: '100vh',
+    width: '100%',
+  },
 });
 
 export default function SignUp() {
   const classes = useStyles();
 
   return(
-    <>
-      <p>This is the sign up page</p>
-    </>
+    <div className={classes.signUpContainer}>
+      <div className={classes.signUpContainerOverlay}>
+
+      </div>
+    </div>
   );
 }
