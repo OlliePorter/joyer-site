c70337b997a22004afb2f6d6fbdf53ece92c3d9d
OlliePorter <ol.porter@me.com>
2020-04-13 10:28:30 +0100

    Try to conditionally render iphone image

--- src/components/Home/CoreFeature.js
+++ src/components/Home/CoreFeature.js
@@ -3,13 +3,14 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import theme from '../../theme';
 import { ReactComponent as IphoneGoals } from '../../assets/iphone_joyage.svg';
+import { ReactComponent as IphoneNotifications } from '../../assets/notifications.svg';
 
 const useStyles = makeStyles({
     title: {
         color: theme.palette.primary.main,
         fontWeight: 600,
         margin: '0px',
-        paddingTop: '150px',
+        paddingTop: '180px',
         [theme.breakpoints.down('md')]: {
             paddingTop: '80px',
             textAlign: 'center',
@@ -21,11 +22,24 @@ const useStyles = makeStyles({
         fontWeight: 300,
         paddingBottom: '8em',
         width: '60%',
-        [theme.breakpoints.down('md')]: {
+        [theme.breakpoints.down('sm')]: {
             paddingBottom: '1em',
             width: '80%',
             padding: '35px',
             textAlign: 'center',
+        },
+        [theme.breakpoints.up('sm')]: {
+            width: '90%',
+        },
+        [theme.breakpoints.up('md')]: {
+            width: '90%',
+            padding: '50px',
+            textAlign: 'center',
+        },
+        [theme.breakpoints.up('lg')]: {
+            width: '60%',
+            padding: '0px',
+            textAlign: 'left',
         },
     },
     coverText: {
@@ -56,20 +70,33 @@ const useStyles = makeStyles({
     }
 });
 
-export default function CoreFeature() {
+export default function CoreFeature(props) {
 const classes = useStyles();
 
+let component = null;
+console.log(props.imageLink);
+switch(props.imageLink) {
+  case "iphone_joyage.svg":
+    component = <IphoneGoals className={classes.iPhone}/>;
+    break;
+  case "iphone_notifications.svg":
+    component = <IphoneNotifications className={classes.iPhone}/>;
+    break;
+  default:
+    component = 'bloop';
+}
+console.log(component);
 return(
-    <div className={classes.coverBackground}>
-        <div classname={classes.coverText}>
-            <Typography component="h2" variant="h3" className={classes.title}>
-            1. Goal Setting
-            </Typography>
-            <Typography component="h2" variant="h6" className={classes.subtitle}>
-            See more friends each week by setting a personalised social goal called a Joyage. For introverts, extroverts & not-a-lot-of-time-troverts, choose a cadence that works for you.
-            </Typography>
+        <div className={classes.coverBackground}>
+            <div>
+                <Typography component="h2" variant="h3" className={classes.title}>
+                {props.title}
+                </Typography>
+                <Typography component="h2" variant="h6" className={classes.subtitle}>
+                {props.body}
+                </Typography>
+            </div>
+            {component}
         </div>
-        <IphoneGoals className={classes.iPhone}/>
-    </div>
     );
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Cover from './Cover';
 import CoreFeature from './CoreFeature';
-import Features from './Features';
 import SignUpForm from './SignUpForm';
 import Footer from './Footer';
 
@@ -9,9 +8,20 @@ export default function Home() {
     return (
     <>
       <Cover/>
-      <CoreFeature/>
+      <CoreFeature
+      title="1. Goal Setting"
+      body="See more friends each week by setting a personalised social goal called a Joyage.
+      For introverts, extroverts & not-a-lot-of-time-troverts, choose a cadence that works for you."
+      imageLink="iphone_joyage.svg"
+      />
+       <CoreFeature
+      title="2. Reminders"
+      body="We'll send you tailored reminders to prompt you to meet your Joyage goal. This enables
+      you to see more of the people you care about for activities you'll never forget."
+      imageLink="iphone_notifications.svg"
+      />
       <SignUpForm/>
       <Footer/>
     </>
-    );
+  );
 }
