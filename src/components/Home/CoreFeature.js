6032183d6f1c529a9da60c9eeef20fa11bb8093c
OlliePorter <ol.porter@me.com>
2020-05-11 22:31:40 +0100

    Try and fix iphone image spacing

--- src/components/Home/CoreFeature.js
+++ src/components/Home/CoreFeature.js
@@ -36,7 +36,6 @@ const useStyles = makeStyles({
             textAlign: 'center',
         },
         [theme.breakpoints.up('lg')]: {
-            width: '70%',
             padding: '0px',
             textAlign: 'left',
             paddingLeft: '50px',
